8b04f5d48c832aa55f8353644dd7f76fb111247c
Loïc Veyssière <loic.veyssiere@gmail.com>
2019-02-15 11:23:20 +0100

    add basic openmp in pcg, ready for raspberry testing

--- 1-cg-sequential-c/include/algebra.h
+++ 1-cg-sequential-c/include/algebra.h
@@ -16,22 +16,22 @@ void init_sparse_coo(sparse_coo* matrix, int length);
 LINEAR ALGEBRA
 ============================================================================= */
 
-double dot_product(double* vec1, double* vec2, int size);
+inline double dot_product(double* vec1, double* vec2, int size);
 
-void matrix_vec_product(sparse_coo* matrix, double* vec, double* result, int size);
+inline void matrix_vec_product(sparse_coo* matrix, double* vec, double* result, int size);
 
-void vec_copy(double* from, double* to, int size);
+inline void vec_copy(double* from, double* to, int size);
 
-void vec_set(double* vec, double value, int start, int end);
+inline void vec_set(double* vec, double value, int start, int end);
 
-void vec_sub(double* vec1, double* vec2, double* result, int size);
+inline void vec_sub(double* vec1, double* vec2, double* result, int size);
 
-void vec_add(double* vec1, double* vec2, double* result, int size);
+inline void vec_add(double* vec1, double* vec2, double* result, int size);
 
-void vec_mul(double* vec1, double* vec2, double* result, int size);
+inline void vec_mul(double* vec1, double* vec2, double* result, int size);
 
-void scalar_mul(double* vec, double scalar, double* result, int size);
+inline void scalar_mul(double* vec, double scalar, double* result, int size);
 
-void vec_affine(double* vec1, double* vec2, double scalar, double* result, int size);
+inline void vec_affine(double* vec1, double* vec2, double scalar, double* result, int size);
 
 #endif
--- 1-cg-sequential-c/src/main.c
+++ 1-cg-sequential-c/src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+//#include <omp.h>
 #include "solver.h"
 #include "algebra.h"
 
@@ -8,9 +9,9 @@
 int main(int argc, char** argv) {
 
     printf("Welcome to Conjugate Gradient\n");
-
+    
     int size = 10000;
-    int length = size + 2*(size-1) + 2*(size-3);
+    int length = size + 2*(size-1); //+ 2*(size-3);
     double tol = 10E-10;
     int max_iter = 3000;
 
@@ -31,21 +32,22 @@ int main(int argc, char** argv) {
     for (int i = 0; i < size; i++) {
         matrix.index_1[k] = i;
         matrix.index_2[k] = i;
-        matrix.values[k] = 4.0;
+        matrix.values[k] = i+1 / (double) size;
         k++;
     }
 
     for (int i = 0; i < size-1; i++) {
         matrix.index_1[k] = i;
         matrix.index_2[k] = i+1;
-        matrix.values[k] = 1.0;
+        matrix.values[k] = -1.0;
         k++;
         matrix.index_1[k] = i+1;
         matrix.index_2[k] = i;
-        matrix.values[k] = 1.0;
+        matrix.values[k] = -1.0;
         k++;
     }
 
+    /*
     for (int i = 0; i < size-3; i++) {
         matrix.index_1[k] = i+2;
         matrix.index_2[k] = i+1;
@@ -56,6 +58,7 @@ int main(int argc, char** argv) {
         matrix.values[k] = 1.0;
         k++;
     }
+    */
 
     printf("%d : %d\n", k, length);
 
